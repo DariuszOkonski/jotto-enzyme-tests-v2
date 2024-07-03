49465b50e10c10e4bf43a3cf497130f2ae0d7798
Dariusz Okoński <darek200180@gmail.com>
2024-07-03 17:22:37 +0200

    testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
+import axios from 'axios';
 import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 import { findByTestAttr } from '../test/testUtils';
 import App from './App';
+import { getSecretWord as mockGetSecretWord } from './actions';
+
+// activate global mock to make sure getSecretWord doesn't make network call
+jest.mock('./actions');
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
@@ -14,4 +19,10 @@ test('renders without error', () => {
   const wrapper = setup();
   const appComponent = findByTestAttr(wrapper, 'component-app');
   expect(appComponent.length).toBe(1);
+});
+
+describe('get secret word', () => {
+  test('getSecretWord on app mount', () => {});
+
+  test('getSecretWord does not run on app update', () => {});
 });
